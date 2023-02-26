e72fe6299e4b20be3fcd60a72116a94095483fe7
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2023-02-26 19:52:15 -0300

    update

--- Curso Em Vídeo - Challenges/desafio001.py
+++ Curso Em Vídeo - Challenges/desafio001.py
@@ -1,6 +1,1 @@
-n1 = int(input('Insira o primeiro número: '))
-n2 = int(input('Insira o segundo número: '))
-
-soma = n1 + n2
-
-print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
+print('Olá, mundo!')
